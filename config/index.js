40353fec406dc33ce89eba45799d65065bafb5b3
chengshuo <chengshuo@winsyo.com>
2018-04-03 15:48:55 +0800

    修改路径位置

--- config/index.js
+++ config/index.js
@@ -58,7 +58,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/ccmanager666',
+    assetsPublicPath: '/ccmanager666/',
 
     /**
      * Source Maps
