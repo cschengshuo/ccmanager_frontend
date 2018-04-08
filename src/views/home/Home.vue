a67300251c736dcd663048b0dfee7a57531a3fc0
chengshuo <chengshuo@winsyo.com>
2018-04-08 21:49:30 +0800

    修改大小写

---
+++ src/views/home/Home.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <a>欢迎你 {{ userName }}</a>
+        <Button type="primary" @click="initRole">初始化角色</Button>
+        <Button type="primary" @click="initUserRole">初始化用户角色</Button>
+    </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+        }
+    },
+    methods: {
+        initRole () {
+            this.$http.post('/api/user/initRole')
+        },
+        initUserRole () {
+            this.$http.post('/api/user/initUserRole')
+        }
+    },
+    mounted () {
+    },
+    computed: {
+        userName () {
+            if (this.$store.state.user) {
+                return this.$store.state.user.name
+            }
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
